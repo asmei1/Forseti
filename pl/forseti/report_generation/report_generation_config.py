2faaa48fa83857b68dd744e680e7fba5048099f3
asmei <kamsykula@gmail.com>
2023-05-06 11:12:24 +0200

    Add new metrics to report generator
    Add possibility to get metrics from report generator

--- pl/forseti/report_generation/report_generation_config.py
+++ pl/forseti/report_generation/report_generation_config.py
@@ -6,6 +6,7 @@ class ReportGenerationConfig:
     generate_heatmap_for_code_units: bool = False
     generate_jsons: bool = True
     generate_html_diffs: bool = True
+    generate_code_unit_metrics: bool = True
     n_processors: int = 1
     minimal_similarity_threshold: float = 0.5
     maximal_similarity_threshold: float = 1.0
--- pl/forseti/report_generation/report_generator.py
+++ pl/forseti/report_generation/report_generator.py
@@ -14,7 +14,6 @@ from .html_diff_generator import generate_html_diff_page
 from .generate_heatmap import generate_heatmap
 from ..utils.multiprocessing import execute_function_in_multiprocesses
 
-
 class ReportGenerator:
     @staticmethod
     def get_length_data(matches):
@@ -113,24 +112,36 @@ class ReportGenerator:
         longest_sequences = []
         for _, results in grouped_results.items():
             similarity = 0.0
+            program_1_overlap_factor = 0.0
+            program_2_overlap_factor = 0.0
             longest_sequence = 0
             for result in results['code_units']:
                 raw = result['raw']
                 aa = np.sum(raw.matches_a)
                 bb = np.sum(raw.matches_b)
                 code_unit_similarity = (aa + bb) / (len(raw.matches_a) + len(raw.matches_b))
+                code_unit_1_overlap_factor = aa / len(raw.matches_a)
+                code_unit_2_overlap_factor = bb / len(raw.matches_b)
+                program_1_overlap_factor += code_unit_1_overlap_factor
+                program_2_overlap_factor += code_unit_2_overlap_factor
                 similarity += code_unit_similarity
                 result['similarity'] = code_unit_similarity
+                result['overlap_1'] = code_unit_1_overlap_factor
+                result['overlap_2'] = code_unit_2_overlap_factor
                 current_longest_match = max(raw.result, key=lambda x: x['length'])['length']
                 result['length'] = current_longest_match
                 longest_sequence = max([longest_sequence, current_longest_match])
             
 
             similarity = similarity / len(results['code_units'])
+            program_1_overlap_factor = program_1_overlap_factor / len(results['code_units'])
+            program_2_overlap_factor = program_2_overlap_factor / len(results['code_units'])
             similarities.append(similarity)
             longest_sequences.append(longest_sequence)
             
             results['similarity'] = similarity
+            results['program_1_overlap'] = program_1_overlap_factor
+            results['program_2_overlap'] = program_2_overlap_factor
             results['length'] = longest_sequence
         
         sd = np.std(np.array(similarities), axis=0)
@@ -148,7 +159,7 @@ class ReportGenerator:
         logging.info(f"Highest similarity: {max(similarities)}" )
         logging.info(f"Sequence length threshold: {longest_sequence_threshold}")
         logging.info(f"Max sequence length: {max(longest_sequences)}" )
-        logging.info(f"Suspected comparisons: {comparisons_number}/{comparisons_number_after_filtration}" )
+        logging.info(f"Suspected submissions: {comparisons_number}/{comparisons_number_after_filtration}" )
 
         return (similarity_threshold, longest_sequence_threshold, grouped_results)
 
@@ -166,15 +177,20 @@ class ReportGenerator:
             os.makedirs(self.__html_output_path) 
 
         
-    def __init__(self, comparison_results: List[ComparisonResult], report_generation_config: ReportGenerationConfig, output_path) -> None:
+    def __init__(self, comparison_results: List[ComparisonResult], report_generation_config: ReportGenerationConfig, output_path=None) -> None:
         self.__comparison_results = comparison_results
         self.__report_generation_config = report_generation_config
         #TODO: name should be changed
-        self.__heatmaps_output_path = os.path.join(output_path, "heatmaps")
-        self.__json_output_path = os.path.join(output_path, "jsons")
-        self.__html_output_path = os.path.join(output_path, "html")
+        if output_path:
+            self.__heatmaps_output_path = os.path.join(output_path, "heatmaps")
+            self.__json_output_path = os.path.join(output_path, "jsons")
+            self.__html_output_path = os.path.join(output_path, "html")
+            self.__create_folders__()
+        else:
+            self.__heatmaps_output_path = None
+            self.__json_output_path = None
+            self.__html_output_path = None
         self.__similarity_threshold, self.__sequence_length_threshold, self.__preprocessed_comparison_results = self.__proces_comparison_results__()
-        self.__create_folders__()
 
 
 
@@ -198,9 +214,18 @@ class ReportGenerator:
         code_unit_info["length"] = len(tokens)
         return code_unit_info
 
+    def __format_data__(self, data):
+        d = {}
+        data.sort(key=lambda x: x[1], reverse=True)
+        for name, raw in data:
+            d[name] = raw
+        return d
+    
     def get_comparison_results(self) -> None:
         results = []
         raw_overalls = []
+        raw_1_overlaps = []
+        raw_2_overlaps = []
         for _, comparison_result in self.__preprocessed_comparison_results.items():
             overall_similarity = comparison_result['similarity']
             if overall_similarity < self.__similarity_threshold:
@@ -217,6 +242,8 @@ class ReportGenerator:
             data_dump["filenames_2"] = pair.program_b.filenames
 
             data_dump["overall_similarity"] = comparison_result['similarity']
+            data_dump["program_1_overlap"] = comparison_result['program_1_overlap']
+            data_dump["program_2_overlap"] = comparison_result['program_2_overlap']
             data_dump["longest_sequence"] = comparison_result['length']
             data_dump["raw_comparison_results"] = []
             for code_unit in code_units:
@@ -226,57 +253,76 @@ class ReportGenerator:
                 data["code_unit_2"] = self.__get_code_unit_info__(pair.tokens_b)
                 data["localization"] = code_unit['raw'].result
                 data["similarity"] = code_unit['similarity']
+                data["overlap_1"] = code_unit['overlap_1']
+                data["overlap_2"] = code_unit['overlap_2']
                 data_dump["raw_comparison_results"].append(data)
 
             raw_overalls.append((name, data_dump["overall_similarity"]))
+            raw_1_overlaps.append((name, data_dump["program_1_overlap"]))
+            raw_2_overlaps.append((name, data_dump["program_2_overlap"]))
             results.append((name, data_dump))
 
-        overalls = {}
-        raw_overalls.sort(key=lambda x: x[1], reverse=True)
-        for name, raw in raw_overalls:
-            overalls[name] = raw
-        results.append(('similarity', overalls))
-        return results             
+        summary = {}
+        summary['similarity'] = self.__format_data__(raw_overalls)
+        summary['program_1_overlap'] = self.__format_data__(raw_1_overlaps)
+        summary['program_2_overlap'] = self.__format_data__(raw_2_overlaps)
+        return (results, summary)             
              
     @staticmethod 
     def dump_comparison_result_in_json(data_pack):
         config, (name, data) = data_pack
-        if name == 'overall':
-            with open(os.path.join(config[0], name + ".json"), 'w') as outfile:
-                json.dump(data, outfile, indent=4)
+        with open(os.path.join(config[0], name + ".json"), 'w') as outfile:
+            json.dump(data, outfile, indent=4)
     
     @staticmethod 
     def dump_comparison_result_in_json_and_html_diff(data_pack):
         config, (name, data) = data_pack
-        # if True:
         json_output_path, html_output_path = config
         path_to_json = os.path.abspath(os.path.join(json_output_path, name + ".json"))
-        path_to_html_diff = os.path.abspath(os.path.join(html_output_path, name + ".html"))
         with open(path_to_json, 'w') as outfile:
             json.dump(data, outfile, indent=4)
-        if name != 'similarity':
-            generate_html_diff_page(path_to_json, path_to_html_diff)
+        path_to_html_diff = os.path.abspath(os.path.join(html_output_path, name + ".html"))
+
+        files_1 = {}
+        files_2 = {}
+        for f in data['filenames_1']:
+            with open(f, 'r', encoding='latin-1') as file:
+                files_1[f] =  file.readlines()
+        for f in data['filenames_2']:
+            with open(f, 'r', encoding='latin-1') as file:
+                files_2[f] =  file.readlines()
+        with open(path_to_html_diff, 'w', encoding='utf8') as file:
+            file.write(generate_html_diff_page(data, files_1, files_2))
 
     def dump_comparison_results_in_json(self):
-        results = self.get_comparison_results()
+        results, summary = self.get_comparison_results()
         config = (self.__json_output_path)
         if self.__report_generation_config.n_processors == 1:
             for name, data_dict in results:
                 ReportGenerator.dump_comparison_result_in_json((config, (name, data_dict)))
         else:
             execute_function_in_multiprocesses(ReportGenerator.dump_comparison_result_in_json, zip([config]*len(results), results), self.__report_generation_config.n_processors)
+        for name, data_dict in summary.items():
+            ReportGenerator.dump_comparison_result_in_json((config, (name, data_dict)))
     
     def dump_comparison_results_in_json_with_html_diffs(self):
-        results = self.get_comparison_results()
+        results, summary = self.get_comparison_results()
         config = (self.__json_output_path, self.__html_output_path)
         if self.__report_generation_config.n_processors == 1:
             for name, data_dict in results:
                 ReportGenerator.dump_comparison_result_in_json_and_html_diff((config, (name, data_dict)))
         else:
             execute_function_in_multiprocesses(ReportGenerator.dump_comparison_result_in_json_and_html_diff, list(zip([config]*len(results), results)), self.__report_generation_config.n_processors)
+        for name, data_dict in summary.items():
+            ReportGenerator.dump_comparison_result_in_json((config, (name, data_dict)))
     
-    def generate_reports(self):
+    def dump_code_unit_metrics(self):
+        metrics = self.generate_code_unit_metrics()
+        path_to_json = os.path.abspath(os.path.join(self.__json_output_path, "code_unit_metrics.json"))
+        with open(path_to_json, 'w') as outfile:
+            json.dump(metrics, outfile, indent=4)
 
+    def generate_reports(self):
         if self.__report_generation_config.generate_heatmap_for_code_units:
             self.dump_heatmap_per_compared_code_units()
 
@@ -287,5 +333,25 @@ class ReportGenerator:
             self.dump_comparison_results_in_json_with_html_diffs()
         elif self.__report_generation_config.generate_jsons:
             self.dump_comparison_results_in_json()
+
+        if self.__report_generation_config.generate_code_unit_metrics:
+            self.dump_code_unit_metrics()
+
+    def generate_metrics(self):
+        _, summary = self.get_comparison_results()
+        return summary
+    
+    def generate_code_unit_metrics(self):
+        results, _ = self.get_comparison_results()
+        metrics = {}
+        for comparison_name, comp in results:
+            for raw in comp['raw_comparison_results']:
+                if raw['similarity'] >= self.__similarity_threshold:
+                    key = raw['code_unit_1']['first_token_name'] + " " + raw['code_unit_2']['first_token_name']
+                    if key not in metrics:
+                        metrics[key] = []    
+                    metrics[key].append((comparison_name, raw['similarity']))
+        return metrics
+        
 
         
