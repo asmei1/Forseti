463a89403c6aaf86f0c6aff985e23d01eb1d2dfd
asmei <kamsykula@gmail.com>
2023-07-21 00:39:21 +0200

    Correct low performance of execute_function_in_multiprocesses  function

--- pl/forseti/utils/multiprocessing.py
+++ pl/forseti/utils/multiprocessing.py
@@ -2,6 +2,6 @@ from multiprocessing import Pool
 import tqdm
 
 
-def execute_function_in_multiprocesses(func, data, n_processors):
+def execute_function_in_multiprocesses(func, data, n_processors, mininterval=0.1, chunksize=1):
     with Pool(processes=n_processors) as pool:
-        return list(tqdm.tqdm(pool.imap(func, data), total=len(data)))
+        return list(tqdm.tqdm(pool.imap_unordered(func, data, chunksize=chunksize), total=len(data), mininterval=mininterval))
